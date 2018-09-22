8ac7f2e95948a1aad86ce8e498902ac777b1296d
Foyn2 <36171130+Foyn2@users.noreply.github.com>
2018-09-22 00:00:11 -0400

    Update MSP430G2553.c

--- Multiple Blink/MSP430G2553.c
+++ Multiple Blink/MSP430G2553.c
@@ -2,25 +2,26 @@
 
 
 /**
- * main.c
+ * The main.c file for the Multiple Blink on the standard G2553 board
+ * Nolan Foy
  */
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
-	P1DIR |= (BIT0|BIT6); //Assigns bits 0 and 6 to be output pins
+	P1DIR |= (BIT0|BIT6); //Assigns bits 0 and 6 to be output pins (OR)
 
 
-	for(;;) //this will loop infinitely
+	for(;;) //An infinite for loop
 	{
-	    volatile unsigned int a, b; //TBD
+	    volatile unsigned int a, b; //Integers that are able to change at any time but will never be negative
 
-	    a++; //Counts i by 1
-	    b++; //Counts j by 1
+	    a++; //Counts a by 1
+	    b++; //Counts b by 1
 
 	    if(a>3000){//Controls blink rate for 1.0 Led; counts up to this number
 	        a = 0; //Sets a to start at 0
-	        P1OUT ^= BIT0; //Tells led to turn on and off (0 and 1)
+	        P1OUT ^= BIT0; //Tells Led to turn on and off (0 and 1)
 	    }
 
 	    if(b>30000)//Controls blink rate for 1.6 Led; counts up to this number
