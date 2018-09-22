2b9a0413478aa3af9ecdd3d2b5d3da9da4116d9c
Foyn2 <36171130+Foyn2@users.noreply.github.com>
2018-09-22 00:11:19 -0400

    Update main.c

--- Off_Board Blink/main.c
+++ Off_Board Blink/main.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main.c
+ * The main.c file for the Off Board Blink. The Multiple Blink code was used to demostrate the LEDs on the breadboard.
+ * Nolan Foy
  */
 int main(void)
 {
@@ -13,16 +14,16 @@ int main(void)
 
 	    for(;;) //this will loop infinitely
 	    {
-	        volatile unsigned int a, b, c; //TBD
+	        volatile unsigned int a, b, c; //Integers that can change at anytime but can never be negative
 
 	        a++; //Counts a by 1
 	        b++; //Counts b by 1
 	        c++; //counts c by 1
 
-	        if(a>3000){//Controls blink rate for 1.0 Led; counts up to this number
+	        if(a>3000){//Controls blink rate for 1.0 LED; counts up to this number
 	            a = 0; //Sets a to start at 0
 	            P1OUT ^= BIT0; //Tells led to turn on and off (0 and 1)
-	            P2OUT ^= BIT2; //
+	            
 	        }
 
 	        if(b>30000)//Controls blink rate for 1.6 Led; counts up to this number
@@ -34,8 +35,8 @@ int main(void)
 
 	        if(c>500000)//Controls blink rate for 2.2 Led;
 	        {
-	            c = 0;
-	            P2OUT ^= BIT2;
+	            c = 0; //Sets c to start at 0
+	            P2OUT ^= BIT2; //Tells Led to turn on and off (0 and 1)
 	        }
 
 	    }
