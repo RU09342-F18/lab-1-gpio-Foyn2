75bc8be968bafc83ea7acc093048521debdf7b5a
Foyn2 <36171130+Foyn2@users.noreply.github.com>
2018-09-21 23:51:11 -0400

    Update MSP430G2553 .c

--- Simple Blink/MSP430G2553 .c
+++ Simple Blink/MSP430G2553 .c
@@ -2,17 +2,18 @@
 
 
 /**
- * main.c
+ * The main.c file for the Simple Blink on the standard G2553 Board. 
+ * Nolan Foy
  */
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 
-	P1DIR |= BIT0; //Sets Bit 0 to be an output pin
+	P1DIR |= BIT0; // ORs P1DIR with Bit 0 to set Bit 0 to be an output pin
 	
-	while(1) //Means while true. The loop will run infinitely unless stated otherwise
+	while(1) //Means while true. The loop will run infinitely unless another coded statement states otherwise
 	{
-	   P1OUT ^= BIT0; //Tells Led to turn on and off by XOR (flipping the bit between 0 and 1; ON and OFF)
+	   P1OUT ^= BIT0; //Tells LED on pin 1.0 to turn on and off by XOR (flipping the bit between 0 and 1; ON and OFF)
 	   __delay_cycles(500000); //Sets a toggle delay of 0.5 seconds
 	}
 
