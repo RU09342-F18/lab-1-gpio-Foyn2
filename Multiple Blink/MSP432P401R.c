a63ee63ba440e2c3841e9128653b698530d4bdb1
Foyn2 <36171130+Foyn2@users.noreply.github.com>
2018-09-22 00:02:51 -0400

    Update MSP432P401R.c

--- Multiple Blink/MSP432P401R.c
+++ Multiple Blink/MSP432P401R.c
@@ -2,29 +2,30 @@
 
 
 /**
- * main.c
+ * The main.c file for the Multiple Blink on the MSP432P401R board
+ * Nolan Foy
  */
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-	P1DIR |= BIT0; //Sets Bit 0 as output pin
+	P1DIR |= BIT0; //Sets Bit 0 from P1.0 as output pin
 
 	P2DIR |= BIT0; //sets bit 0 from P2.0 as output pin
 
 	for(;;) //this will loop infinitely
 	    {
-	        volatile unsigned int a, b;
+	        volatile unsigned int a, b; //Integers that are able to change at anytime but will never be negative 
 
-	        a++; //Counts i by 1
-	        b++; //Counts j by 1
+	        a++; //Counts a by 1
+	        b++; //Counts b by 1
 
 	        if(a>7000){//Controls blink rate for 1.0 Led; counts up to this number
 	            a = 0;
 	            P1OUT ^= BIT0; //Tells led to turn on and off (0 and 1)
 	        }
 
-	        if(b>70000)//Controls blink rate for 1.6 Led; counts up to this number
+	        if(b>70000)//Controls blink rate for 2.0 Led; counts up to this number
 	        {
 	            b = 0;
 	            P2OUT ^= BIT0; //Tells Led to turn on and off (0 and 1)
