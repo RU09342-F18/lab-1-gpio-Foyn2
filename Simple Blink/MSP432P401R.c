ac01f8fea3b028f02a3db1f007695cfb1b0a2b38
Foyn2 <36171130+Foyn2@users.noreply.github.com>
2018-09-21 23:54:16 -0400

    Update MSP432P401R.c

--- Simple Blink/MSP432P401R.c
+++ Simple Blink/MSP432P401R.c
@@ -2,16 +2,17 @@
 
 
 /**
- * main.c
+ * The main.c file for the Simple Blink on the MSP432P401R board
+ * Nolan Foy
  */
 void main(void) //sub main clock
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
 
-	P1DIR |= BIT0; //sets bit 0 as an output pin
+	P1DIR |= BIT0; //ORs P1DIR (Direction Register) with bit 0 to set bit 0 as an output pin
 
-	while(1){//Infinite while loop
-	    P1OUT ^= BIT0; //Flip flops the Led between a 1 and 0
+	while(1){ // Means while true, indicating an infinite while loop
+	    P1OUT ^= BIT0; //Flip flops the LED between a 1 and 0 by virtue of XOR
 	    _delay_cycles(500000); // Toggle delay of 0.5 seconds
 	}
 	return 0;
